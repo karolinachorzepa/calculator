bc19d2802e5a9d944559f926a9ae7f12a6b17b23
Karolina Chorzepa <karolina.chorzepa@o2.pl>
2020-12-15 18:08:04 +0100

    second_nr_outside_if

--- calculator.py
+++ calculator.py
@@ -5,23 +5,20 @@ logging.basicConfig(level=logging.DEBUG)
 result = None
 firstnumber = float(input("Podaj liczbę: "))
 matematical_operation =int(input("Podaj operację matematyczną: 1 dla (+), 2 dla (-), 3 dla (*), 4 dla (/) "))
+secondnumber = float(input("Podaj 2 liczbę: "))
 if matematical_operation == 1: 
-    secondnumber = float(input("Podaj 2 liczbę: "))
     logging.info(f"Dodaję {firstnumber} i {secondnumber}")
     result = firstnumber + secondnumber
 elif matematical_operation == 2: 
-    secondnumber = float(input("Podaj 2 liczbę: "))
     logging.info(f"Odejmuję {firstnumber} i {secondnumber}")
     result = firstnumber - secondnumber
 elif matematical_operation == 3: 
-    secondnumber = float(input("Podaj 2 liczbę: "))
     logging.info(f"Mnożę {firstnumber} i {secondnumber}")
     result = firstnumber * secondnumber
 elif matematical_operation == 4: 
-    secondnumber = float(input("Podaj 2 liczbę: "))
     logging.info(f"Dzielę {firstnumber} i {secondnumber}")
     result = firstnumber / secondnumber
 else:
     logging.error("Wybrano niepoprawną operację matematyczną")
-    exit(1)
+    exit(0)
 print(f"Wynik działania to {result}")
