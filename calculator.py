37ea49b08d323dce8357b94fff063c8cb12d8726
Karolina Chorzepa <karolina.chorzepa@o2.pl>
2020-12-15 15:51:14 +0100

    Import logging

--- calculator.py
+++ calculator.py
@@ -1,20 +1,27 @@
+import sys
 import logging
+logging.basicConfig(level=logging.DEBUG)
+
 result = None
 firstnumber = float(input("Podaj liczbę: "))
 matematical_operation =int(input("Podaj operację matematyczną: 1 dla (+), 2 dla (-), 3 dla (*), 4 dla (/) "))
 if matematical_operation == 1: 
-    secondnumber = int(input("Podaj 2 liczbę: "))
+    secondnumber = float(input("Podaj 2 liczbę: "))
+    logging.info(f"Dodaję {firstnumber} i {secondnumber}")
     result = firstnumber + secondnumber
-elif matematical_operation == 2:
-    secondnumber = int(input("Podaj 2 liczbę: "))
+elif matematical_operation == 2: 
+    secondnumber = float(input("Podaj 2 liczbę: "))
+    logging.info(f"Odejmuję {firstnumber} i {secondnumber}")
     result = firstnumber - secondnumber
-elif matematical_operation == 3:
-    secondnumber = int(input("Podaj 2 liczbę: "))
+elif matematical_operation == 3: 
+    secondnumber = float(input("Podaj 2 liczbę: "))
+    logging.info(f"Mnożę {firstnumber} i {secondnumber}")
     result = firstnumber * secondnumber
-elif matematical_operation == 4:
-    secondnumber = int(input("Podaj 2 liczbę: "))
+elif matematical_operation == 4: 
+    secondnumber = float(input("Podaj 2 liczbę: "))
+    logging.info(f"Dzielę {firstnumber} i {secondnumber}")
     result = firstnumber / secondnumber
 else:
-    print("Wybrano niepoprawną operację matematyczną")
+    logging.error("Wybrano niepoprawną operację matematyczną")
     exit(1)
-print(result)
+print(f"Wynik działania to {result}")
